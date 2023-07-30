8d96992615909848b0bb1b5935d7352562554456
Fer G. Vargas <fernandadguimaraes@hotmail.com>
2023-07-30 10:40:11 -0300

    Trabalho 3

---
+++ 3 - Projeto React_JavaScript/loja_virtual/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <header></header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 3 - Projeto React_JavaScript/loja_virtual/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="header" style={{background: 'red'}}>
+        <div className="container"style={{maxWidth:'800px', margin: "0 auto"}}>
+            Fernanda
+        </div>
+        </header>
+    );
+}
+
+export default Header
